0c9f40391107c83f106014648834389d4c6ac8a9
Jim McGee <jim.mcgee@socialtables.com>
2018-03-09 18:10:40 -0600

    remove scene context

--- src/components/Game.js
+++ src/components/Game.js
@@ -19,14 +19,6 @@ class Game extends React.Component {
     children: PropTypes.node,
   };
 
-  static childContextTypes = {
-    scene: PropTypes.object,
-  };
-
-  getChildContext() {
-    return { scene: this.scene };
-  }
-
   componentDidMount() {
     const {
       width,
